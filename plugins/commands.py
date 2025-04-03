97aa5b3c2892365e105abcaa3e90fb7675e462ec
Pavan844 <117534484+Pavan844@users.noreply.github.com>
2025-04-03 15:20:31 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -1006,7 +1006,7 @@ async def refer(bot, message):
         InlineKeyboardButton(f'⏳ {referdb.get_refer_points(message.from_user.id)}', callback_data='ref_point'),
         InlineKeyboardButton('Close', callback_data='close_data')
     ]]  
-    m=await message.reply_sticker("CAACAgQAAxkBAAEkt_Rl_7138tgHJdEsqSNzO5mPWioZDgACGRAAAudLcFGAbsHU3KNJUx4E")      
+    m=await message.reply_sticker("CAACAgUAAxkBAAEBKvRnRzWGVdZDR6SdYAvdEIoX3MqFtgACowAD7xR5MGsgg9doKpQmHgQ")      
     await m.delete()
     reply_markup = InlineKeyboardMarkup(btn)
     await message.reply_photo(
